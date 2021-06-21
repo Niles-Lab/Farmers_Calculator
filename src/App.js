00c50a92bcea1543c9533b155db95b328409bfde
chiefton117 <timjharrold@gmail.com>
2021-06-21 12:45:03 -0400

    - Added multiple inputs - Fixed input display - Implemented React-Bootstrap - Fixed style
    - Now implementing multiple select dropdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import CalcForm from './Form.js'
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to sdfreload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+
+        </header>
+        <div className="container-fluid">
+          <div>
+            <CalcForm />
+          </div>
+        </div>
+
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,44 +1,116 @@
-class Form extends React.Component {
+//import React, {Component} from 'react';
+import React from "react"
+import { Form, Dropdown, ButtonGroup } from 'react-bootstrap';
+
+class CalcForm extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			acres: 5;
-			dairy: false;
-			name: "Nedrick";
-		}	
-	};
+			acres: 5,
+			dairy: false,
+			name: "Nedrick",
+			options: ["a", "b", "c", "d"]
+		};	
 
 	this.handleInputChange = this.handleInputChange.bind(this);
 
 }
 
-this.setState({
-	[name]: value;
-})
 
-this.handleInputChange(event) {
+
+handleInputChange(event) {
+	console.log(event);
 	const target = event.target;
-	const value = target.type
+	const value = target.type === 'checkbox' ? target.checked : target.value;
 	const name = target.name;
+	const result = target.type === 'submit' ? true : false;
+
+	this.setState({
+	[name]: value
+	});
 }
 
 render() {
 	return (
+
+
 		<form>
-			<label>
+	
+
+			<h1>Calculator</h1>
+			<hr>
+			</hr>
+
+			{
+			// Numerical input
+			}
+
+			<label className="row">
 				Acres:
 				<input
 					name="acres"
 					type="number"
 					value={this.state.acres}
 					onChange={this.handleInputChange} />
+			</label>
 
+			{
+			// Check box
+			}
+
+			<label className="row">
+				Dairy
+				<input
+					name="dairy"
+					type="checkbox"
+					value={this.state.dairy}
+					onChange={this.handleInputChange} />
+			</label>
+
+			{
+			// Multi-select for different techniques
+			}
+			
+			<label className="row">
+				Calculate Costs For:
+				<Form.Control as="select" multiple
+					className="form-control"
+					name="costs"
+					type="select"
+					value={this.state.costs}
+					onChange={this.handleInputChange}>
+					{this.state.options.map(option => ( // Map state options to multi-select
+						<option key={option} value={option}>
+							{option}
+						</option>
+					))}
+				</Form.Control>
+			</label>
+
+			<Dropdown as={ButtonGroup}>
+
+
+
+			</Dropdown>
+			
+			{
+			// Submit for calculation
+			}
+
+			<label>
+				<button
+					className="btn btn-primary"
+					name="submit"
+					type="submit"
+					onChange={this.handleInputChange}>
+					Submit
+				</button>
+			</label>
+		</form>
 
 		)
 }
-ReactDOM.render(
-	<Reservation />,
-	document.getElementById('root')
-	);
 
-export default Form;
+
+}
+export default CalcForm;

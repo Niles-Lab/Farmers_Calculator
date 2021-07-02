4edef182559290449b4114609ee50b1d7252dc7f
Tim Harrold <timjharrold@gmail.com>
2021-07-02 12:42:15 -0400

    Table!
    - Created table output for calculator
    - Added helper methods for individual calculation
    - Wrapped calculator output in a container
    - Briefly documented class functionality

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,7 +1,43 @@
 import React from "react"
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container, Table } from 'react-bootstrap';
+
+
+const opts = ["", "Unit of measure", "WP", "WOP"]
+
 
 class Calculator extends React.Component {
+
+// Main method for calculations - this should be called on render to calculate all table cells
+// Each individual calculation method will output a tuple of [Title(Unit), Value With Project(WP), Value Without Project(WOP)]
+// e.g costPerTree() may return ["Cost Per Tree(USD)", "$5", "$2"]
+calculate() {
+
+}
+
+costPerTree() {
+
+}
+costPerTreeYr() {
+
+}
+returnPerTree() {
+
+}
+returnPerTreeYr() {
+
+}
+profitPerTree() {
+
+}
+profitPerTreeYr() {
+
+}
+totalProjArea() {
+
+}
+npv() {
+
+}
 
 constructor(props) {
 	super(props);
@@ -18,16 +54,41 @@ render() {
 
 	this.state = this.props.vals;
 	return (
+	<Container>
+		<Row>
+			<Col>
+				<div>
+					<p>{parseFloat(this.state.land) + parseFloat(this.state.acres)} Total Land!</p>
+					<p>{this.state.crops.reduce((a,v) => a + parseFloat(v.amount), 0)} Acres Total</p>
 
-	<Row>
-		<Col>
-			<div>
-				<p>{parseFloat(this.state.land) + parseFloat(this.state.acres)} Total Land!</p>
-				<p>{this.state.crops.reduce((a,v) => a + parseFloat(v.amount), 0)} Acres Total</p>
-
-			</div>
-		</Col>
-	</Row>
+				</div>
+			</Col>
+		</Row>
+		<Row>
+			<Table responsive variant="dark">
+				<thead>
+					<tr>
+						<td>1</td>
+					{Array.from({ length: 12 }).map((_, index) => (
+						<td key={index}>Table cell {index}</td>
+						))}	
+					</tr>
+					<tr>
+						<td>1</td>
+					{Array.from({ length: 12 }).map((_, index) => (
+						<td key={index}>Table cell {index}</td>
+						))}	
+					</tr>
+					<tr>
+						<td>2</td>
+					{Array.from({ length: 12 }).map((_, index) => (
+						<td key={index}>Table cell {index}</td>
+						))}	
+					</tr>
+				</thead>
+			</Table>
+		</Row>
+	</Container>
 
 
 		)

a43e8aa903ee86cddfeeb9d1dea7401e58e8bb5a
Tim Harrold <timjharrold@gmail.com>
2021-07-17 23:00:44 -0400

    Fixed Dropdown Checkbox

--- src/calc/DDSelect.jsx
+++ src/calc/DDSelect.jsx
@@ -9,56 +9,46 @@ function DDSelect(props) {
 
 let method = [];
 
-//const [method, setMethod] = useState(data.method);
 
-
+// Adjust internal method
 function adjustMethods(target) {
 	
 	method.indexOf(target.value) > -1 ?
 		method = method.filter((d) => target.value !== d) :
 		method.push(target.value);
 
+	props.setMethod(method);
 }
 
 return (
 
-	<Dropdown as={ButtonGroup}
->
+	<Dropdown as={ButtonGroup}>
 		<DropdownButton 
 		id="dropdown-basic-button" 
 		title="Select"
+		drop={"up"}
 >
-
-{/*			style={{
-				overflow: "hidden",
-				position:"absolute",
-				width: "100%"}}*/}
-			<Dropdown.Divider />
-			<Dropdown.Divider />
-
-				{Object.entries(props.options).map(opts => ( // Map state options to multi-select
-					<span key={opts[0]}>
-						<Dropdown.Item>
-							<b>{opts[0]}</b>
-						</Dropdown.Item>
-							{opts[1].map(d => ( // Map each category's options
-
-								<Form.Check
-								value={d}
-								key={d} 
-								name="costs"
-								type="checkbox"
-								label={d}
-								onChange=
-								{(event) => {adjustMethods(event.target)}}
-	
-									/>
-
-								))}
-						<Dropdown.Divider />
-						<Dropdown.Divider />
-					</span>
+		<Dropdown.Divider />
+		<Dropdown.Divider />
+			{Object.entries(props.options).map(opts => ( // Map state options to multi-select
+				<span>
+				<b key={opts[0]}>{opts[0]}</b>
+				{opts[1].map(d => ( // Map each category's options
+					<Form.Check
+					value={d}
+					className="mx-2"
+					key={d} 
+					name="costs"
+					type="checkbox"
+					label={d}
+					onChange={(event) => {adjustMethods(event.target)}}
+					/>
 				))}
+				<Dropdown.Divider />
+				<Dropdown.Divider />
+
+				</span>
+			))}
 		</DropdownButton>
 	</Dropdown>
 
